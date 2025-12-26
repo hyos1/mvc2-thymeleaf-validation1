67b9f32e8fe62d09bb0139186df83eaaefb8e4bd
hyos <hyozzang99@gmail.com>
2025-12-26 23:56:44 +0900

    Refactor: BindingResult의 FieldError 직접 생성 대신 BindingResult reject/rejectValue 적용

--- src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
+++ src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.validation.DefaultMessageCodesResolver;
 import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.ObjectError;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -20,14 +21,12 @@ public class MessageCodesResolverTest {
     @Test
     void messageCodesResolverField() {
         String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
-        for (String messageCode : messageCodes) {
-            System.out.println("messageCode = " + messageCode);
-        }
         assertThat(messageCodes).containsExactly(
                 "required.item.itemName",
                 "required.itemName",
                 "required.java.lang.String",
                 "required"
         );
+
     }
 }
