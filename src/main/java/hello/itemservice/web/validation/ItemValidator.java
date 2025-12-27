2db4f6021002fcb8861b62f7635fe2a51e0eddbb
hyos <hyozzang99@gmail.com>
2025-12-27 23:58:22 +0900

    Refactor: 컨트롤러에서 검증로직 분리

--- src/main/java/hello/itemservice/web/validation/ItemValidator.java
+++ src/main/java/hello/itemservice/web/validation/ItemValidator.java
@@ -8,28 +8,27 @@ import org.springframework.validation.Validator;
 
 @Component
 public class ItemValidator implements Validator {
+
     @Override
     public boolean supports(Class<?> aClass) {
         return Item.class.isAssignableFrom(aClass);
     }
 
     @Override
-    public void validate(Object o, Errors errors) {
-        Item item = (Item) o;
+    public void validate(Object target, Errors errors) {
+        Item item = (Item) target;
 
         if (!StringUtils.hasText(item.getItemName())) {
             errors.rejectValue("itemName", "required");
         }
-
         if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
             errors.rejectValue("price", "range", new Object[]{1000, 1000000}, null);
         }
-
         if (item.getQuantity() == null || item.getQuantity() >= 9999) {
             errors.rejectValue("quantity", "max", new Object[]{9999}, null);
         }
 
-        // 특정 필드가 아닌 복합 룰 검증
+        //특정 필드가 아닌 복합 룰 검증
         if (item.getPrice() != null && item.getQuantity() != null) {
             int resultPrice = item.getPrice() * item.getQuantity();
             if (resultPrice < 10000) {
